764d1ddb53ce5ac082579c3f9b93a7348b91f1f9
DamianosMav <damianos-212@hotmail.com>
2025-02-16 16:12:27 +0200

    Updated the main script and added the player object

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
+from player import Player
 from constants import *
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
-    while(True):
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawble = pygame.sprite.Group()
+    Player.containers = (updatable, drawble)
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(000000)
+
+        screen.fill("black")
+        updatable.update(dt)
+
+        for obj in drawble:
+            obj.draw(screen)
+            
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
